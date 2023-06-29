6ad99810d13cdba3853f803db6c17f5b96a22c89
carloseduvillegas <carlosedu.villegas@gmail.com>
2023-06-28 20:32:05 -0400

    Modificando estadisticas 2

--- src/tecnogrupo04sa/Presentacion/Help.java
+++ src/tecnogrupo04sa/Presentacion/Help.java
@@ -11,7 +11,7 @@ package tecnogrupo04sa.Presentacion;
  */
 public class Help {
     public static final String UserSupport = "-----------Menu de Ayuda-----------"+ "\n"
-			+ " --------------USUARIOS balusiño-------------- "+ "\n"
+			+ " --------------USUARIOS balusiño gordiño-------------- "+ "\n"
                         + "registrar_paciente"+ "\n"
                         + "CI;NOMBRE;APELLIDO;SEXO;TELEFONO;DIRECCION;FECHA_NAC;" + "\n" 
                         + "\n"
